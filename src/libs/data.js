6d76173fa4b4e8b7c6339203ea5321dfd4f27359
Tom Yotwongjai <munch2018@gmail.com>
2022-07-11 17:21:15 +0200

    delete broken projects

--- src/libs/data.js
+++ src/libs/data.js
@@ -83,21 +83,8 @@ export const projects = [
       'callback function',
     ],
   },
-
   {
     id: 4,
-    name: 'ECommerce CRUD application',
-    descriptions: [
-      'This is CRUD application I did for my junior test at Scandiweb. The applcation use react as front end and PHP/Mysqql for databse. The project inculde form validation.',
-    ],
-
-    deployed_url: 'https://scandiweb-test-bice.vercel.app',
-    github_url: 'https://github.com/tomyotwongjai/scandiweb-test',
-    category: ['previous'],
-    key_techs: ['React', 'PHP', 'MYSQL'],
-  },
-  {
-    id: 5,
     name: 'Chat Application',
     descriptions: [
       'Fully functional end to end chat application using chat.io API, google authentication. Using firebase as a backend',
@@ -109,7 +96,7 @@ export const projects = [
     key_techs: ['React', 'Chat.io api', 'react-hook'],
   },
   {
-    id: 6,
+    id: 5,
     name: 'Static landing page',
     descriptions: [
       'Simple responsive static Landing page using html/css and some javascript for animation & scrollup ',
@@ -117,41 +104,6 @@ export const projects = [
 
     deployed_url: 'https://onepagelanding.netlify.app',
     github_url: 'https://github.com/tomyotwongjai/landingpage-sample',
-    category: ['previous'],
-    key_techs: [
-      'React',
-      'openweathercit api',
-      'react-hook',
-      'callback function',
-    ],
-  },
-  {
-    id: 7,
-    name: 'Weather Application',
-    descriptions: [
-      'Weather application build with react. Fetching current weather information using openweather API ',
-    ],
-
-    deployed_url: 'https://whatweathercity.netlify.app/',
-    github_url: 'https://github.com/tomyotwongjai/react-weather-app',
-    category: ['previous'],
-    key_techs: [
-      'React',
-      'openweathercit api',
-      'react-hook',
-      'callback function',
-    ],
-  },
-
-  {
-    id: 8,
-    name: 'First Portfolio',
-    descriptions: [
-      'Weather application build with react. Fetching current weather information using openweather API ',
-    ],
-
-    deployed_url: 'https://tomyotwongjai.netlify.app/',
-    github_url: 'https://github.com/tomyotwongjai/portfolio',
     category: ['previous'],
     key_techs: [
       'React',
